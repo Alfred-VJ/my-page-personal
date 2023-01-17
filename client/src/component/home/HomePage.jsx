e846c15e7de9cb75680d09252f31e9177e05bf8a
Alfredo Velázquez <alfredtransgenia@gmail.com>
2023-01-17 12:18:37 -0600

    last_change

--- client/src/component/home/HomePage.jsx
+++ client/src/component/home/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../styles/homePage.css';
 import GridHome from '../../grids/gridHome/GridHome';
-import logo_text from '../../assets/Room4.png'
+import photo from '../../assets/derecha.png'
 
 const HomePage = () => {
   return (
     <div className='content_page'>
         <div className='contenido_page'>
           <GridHome />
-          {/* <img src={logo_text} alt="logo" className='logo_text_home'/> */}
+          <img src={photo} alt="logo" className='photo_home'/>
           {/* <h1 className='h1_home'>Un genio es 10% talento y 90% esfuerzo</h1> */}
         </div>
         <div className='lateral_page'>
