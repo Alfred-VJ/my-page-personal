f3c8ca1b305304e6876baa7743d3cbf209fee367
Alfredo Velázquez <alfredtransgenia@gmail.com>
2023-01-22 21:25:08 -0600

    change-background

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@
 import './App.css';
 import HomePage from './component/home/HomePage';
 import NavbarComponent from './component/navbar/NavbarComponent';
-// import Grid from './grids/gridHome/Grid';
+import backGround from './assets/pc_game_red.jpeg';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
       <HomePage />
-      
+     <div>
+         <img src={backGround} alt="" className='img_background'/>
+     </div>
     </div>
   );
 }
--- client/src/component/home/HomePage.jsx
+++ client/src/component/home/HomePage.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import '../styles/homePage.css';
 import GridHome from '../../grids/gridHome/GridHome';
-import photo from '../../assets/derecha.png'
 
 const HomePage = () => {
   return (
     <div className='content_page'>
         <div className='contenido_page'>
           <GridHome />
-          {/* <img src={photo} alt="logo" className='photo_home'/>
-          <h1 className='h1_home'>Un genio es 10% talento y 90% esfuerzo</h1> */}
         </div>
-        <div className='lateral_page'>
-
-        </div>
-        <div className='contenido_page2'>
-          
-          </div>
-          <div className='lateral_page2'>
-          
-          </div>
     </div>
   )
 }
